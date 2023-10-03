04e22462f00fc3d57a673248c90e81db973d9a05
skw398 <skw398@gmail.com>
2023-10-03 14:09:48 +0900

    Initial Commit

---
+++ DoubleHashing/DoubleHashingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleHashingApp.swift
+//  DoubleHashing
+//
+//  Created by Shigenari Oshio on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct DoubleHashingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
